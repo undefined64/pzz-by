56f48bc59b8a0b6ae14ab84e5a5c7c11a3a250fc
undefined64 <kirillpretty@gmail.com>
2024-03-17 12:02:18 +0300

    feat: add pizzas category switcher layout

--- src/Components/PIzzaCard/PizzaCard.tsx
+++ src/Components/PIzzaCard/PizzaCard.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { FiPlus } from 'react-icons/fi';
-
 interface Props {
   imageUrl: string;
   title: string;
@@ -11,7 +10,6 @@ interface Props {
 const PizzaCard = ({ imageUrl, title, price, description }: Props) => {
   const [activeSize, setActiveSize] = useState(0);
   const [activeType, setActiveType] = useState(0);
-  // const categories = ['мясные', 'вегетарианские', 'острые', 'барбекю'];
 
   const handleSizeChange = (index: number) => {
     setActiveSize(index);
@@ -22,7 +20,7 @@ const PizzaCard = ({ imageUrl, title, price, description }: Props) => {
   };
 
   return (
-    <div className="max-w-72 border-lightgray rounded-xl border flex flex-col justify-between px-2 py-4 hover:shadow-md transition ease-out duration-200 mr-5 mb-5">
+    <div className="max-w-72 border-lightgray rounded-xl border flex flex-col justify-end px-2 py-4 hover:shadow-md transition ease-out duration-200 mr-5 mb-5">
       <img
         src={imageUrl}
         alt=""
@@ -30,7 +28,7 @@ const PizzaCard = ({ imageUrl, title, price, description }: Props) => {
       />
       <p className="text-2xl text-primary font-medium mb-0.5">{title}</p>
       <p className="leading-5 mb-3">{description}</p>
-      <p>
+      <p className="mt-auto">
         <ul className="flex bg-lightgray rounded-xl mb-2 cursor-pointer">
           {[25, 30, 35].map((size, index) => (
             <li
@@ -63,7 +61,7 @@ const PizzaCard = ({ imageUrl, title, price, description }: Props) => {
         </ul>
       </p>
       <div className="flex justify-between">
-        <p className="text-2xl ml-1.5 self-center">{price}руб</p>
+        <p className="text-2xl ml-1.5">{price}руб</p>
         <button className="px-4 py-2 bg-primarylight rounded-xl hover:bg-primary flex items-center gap-x-1 active:translate-y-0.5 transition ease-out duration-200">
           В корзину <FiPlus />
         </button>
--- src/Components/PizzaCategories/PizzaCategories.tsx
+++ src/Components/PizzaCategories/PizzaCategories.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
 const PizzaCategories = () => {
-  return <div>PizzaCategories</div>;
+  const categories = ['все', 'мясные', 'вегетарианские', 'острые', 'барбекю'];
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  return (
+    <div className="flex justify-center mb-5">
+      <ul className="flex gap-x-6">
+        {categories.map((category, index) => (
+          <li
+            className={
+              activeCategory === index
+                ? 'text-xl px-4 py-2 rounded-2xl bg-primary cursor-pointer'
+                : 'text-xl px-4 py-2 rounded-2xl bg-lightgray cursor-pointer'
+            }
+            key={index}
+            onClick={() => setActiveCategory(index)}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PizzaCategories;
