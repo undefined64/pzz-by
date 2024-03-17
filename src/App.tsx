8d41169713c0f1d082574b58f27183919a7fcafc
undefined64 <kirillpretty@gmail.com>
2024-03-17 16:29:27 +0300

    feat: add pages for not implemented product categories

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navigation from './Components/Navigation/Navigation';
 import Pizzas from './Pages/Pizzas/Pizzas';
+import Cart from './Pages/Cart/Cart';
 
 function App() {
   return (
     <div className="">
       <Header />
       <Navigation />
-      <Pizzas />
+      {/* <Pizzas /> */}
+      <Cart />
     </div>
   );
 }
---
+++ src/Pages/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+const Cart = () => {
+  return (
+    <div className="container mx-auto w-custom font-exo">
+      <h1 className="text-center text-3xl mt-3">Ваша Корзина</h1>
+      <div>content</div>
+      <p>total: 666$</p>
+    </div>
+  );
+};
+
+export default Cart;
