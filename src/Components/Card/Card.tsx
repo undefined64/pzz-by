fd545aaab8c3757154fa1b8e38bf4d1f07db17a1
undefined64 <kirillpretty@gmail.com>
2024-04-09 19:32:02 +0300

    fix: fix vertical align burger card styles

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Card = ({ imageUrl, title, price, description, weight }: Props) => {
   return (
-    <div className="min-w-72 border-lightgray rounded-xl border flex flex-col justify-end px-2 py-4 hover:shadow-md transition ease-out duration-200 mr-5 mb-5">
+    <div className="min-w-72 border-lightgray rounded-xl border flex flex-col px-2 py-4 hover:shadow-md transition ease-out duration-200 mr-5 mb-5">
       <img
         src={imageUrl}
         alt=""
@@ -18,7 +18,7 @@ const Card = ({ imageUrl, title, price, description, weight }: Props) => {
       <p className="text-2xl text-primary font-medium mb-0.5">{title}</p>
       <p className="leading-5 mb-2">{description}</p>
       <p className="leading-5 mb-2">Вес: {weight}г</p>
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-auto">
         <p className="text-2xl ml-1.5">{price}руб</p>
         <button className="px-4 py-2 bg-primarylight rounded-xl hover:bg-primary flex items-center gap-x-1 active:translate-y-0.5 transition ease-out duration-200">
           В корзину
